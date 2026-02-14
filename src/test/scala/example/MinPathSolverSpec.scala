1e3f3942a8ec69956842660f36a1f4aaf9d9cdeb
arlipo <giga.tjom@gmail.com>
2026-02-14 14:25:21 +0200

    tests

--- src/test/scala/example/MinPathSolverSpec.scala
+++ src/test/scala/example/MinPathSolverSpec.scala
@@ -19,4 +19,34 @@ class MinPathSolverSpec extends CatsEffectSuite {
         assertEquals(result.sum, 18L)
       }
   }
+
+  test("fails when input is empty") {
+    interceptMessageIO[IllegalArgumentException]("Triangle must contain at least one row") {
+      MinPathSolver.minimalPath[IO](Nil).void
+    }
+  }
+
+  test("fails when a row has wrong length") {
+    val malformed = List(
+      "7",
+      "6",
+      "3 8 5"
+    )
+
+    interceptMessageIO[IllegalArgumentException]("Row 2 should contain 2 numbers but found 1") {
+      MinPathSolver.minimalPath[IO](malformed).void
+    }
+  }
+
+  test("fails when a token is not a number") {
+    val malformed = List(
+      "7",
+      "6 two",
+      "3 8 5"
+    )
+
+    interceptMessageIO[IllegalArgumentException]("Invalid number 'two' on row 2") {
+      MinPathSolver.minimalPath[IO](malformed).void
+    }
+  }
 }
